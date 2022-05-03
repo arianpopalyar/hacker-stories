fc652e59aedba262a4b39cd1c2a0ee168ec72394
arianpopalyar <arian_popalyar@yahoo.com>
2022-05-03 22:16:37 +0100

    added more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
-// import { render, screen } from '@testing-library/react';
-
+import axios from 'axios';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App, { Item, List, SearchForm, InputWithLabel } from './App';
@@ -40,6 +39,26 @@ describe('Item', () => {
     expect(handleRemoveItem).toHaveBeenCalledWith(item);
 
     expect(component.root.findAllByType(Item).length).toEqual(1);
+  });
+});
+
+describe('List', () => { const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+    points: 4,
+    objectID: 0,
+    }, {
+    title: 'Redux',
+    url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+    points: 5,
+    objectID: 1,
+    }, 
+  ];
+
+  it('renders two items', () => {
+    const component = renderer.create(<List list={list} />);
+    expect(component.root.findAllByType(Item).length).toEqual(2); 
   });
 });
 
@@ -88,5 +107,56 @@ describe('SearchForm', () => {
 
     expect(
       component.root.findByType('button').props.disabled).toBeTruthy();
+  });
+});
+
+describe('App', () => {
+  it('succeeds fetching data with a list', async () => {
+    const list = [
+      {
+        title: 'React',
+        url:'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      },
+      {
+        title: 'Redux',
+        url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+        points: 5,
+        objectID: 1,
+      }, 
+    ];
+
+    const promise = Promise.resolve({
+      data: {
+        hits: list,
+      },
+    });
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    let component;
+    
+    await renderer.act(async () => {
+      component = renderer.create(<App />);
+    });
+    expect(component.root.findByType(List).props.list).toEqual(list);
+  });
+
+  it('fails fetching data with a list', async () => {
+    const promise = Promise.reject();
+
+    axios.get.mockImplementationOnce(() => promise);
+    let component;
+
+    await renderer.act(async () => {
+      component = renderer.create(<App />);
+    });
+
+    expect(component.root.findByType('p').props.children).toEqual(
+      'Something went wrong ...'
+    )
   });
 });
